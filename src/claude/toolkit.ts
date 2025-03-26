220955d6a6ccd67e79644caef8af937909a72380
luizpaybyrd <luiz@paybyrd.com>
2025-03-26 15:40:32 +0100

    Update toolkit.ts

--- src/claude/toolkit.ts
+++ src/claude/toolkit.ts
@@ -16,7 +16,7 @@ class PaybyrdAgentToolkit extends McpServer {
   }) {
     super({
       name: 'Paybyrd',
-      version: '0.1.2'      
+      version: '0.1.3'      
     });
 
     this._paybyrd = new PaybyrdAPI(apiKey, configuration.context);
